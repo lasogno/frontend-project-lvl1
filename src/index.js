b1e4ad2ff66f22c5e6be34708ede247cd994c50b
Alisa Lobanova <17april94@gmail.com>
2022-05-25 23:26:06 +0300

    add index.js and fix cli.js

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// файл с логикой игр
+import readlineSync from 'readline-sync';
+
+const gamelogic = (rules, gameCondition) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`${rules}`);
+
+  let counter = 0;
+  while (counter < 3) {
+    const [askQuestion, correctAnswer] = gameCondition();
+    console.log(`Question: ${askQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (Number(userAnswer) === correctAnswer) {
+      console.log('Correct!');
+      counter += 1;
+    } else if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    } else {
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+  }
+  if (counter === 3) console.log(`Congratulations, ${userName}!`);
+};
+
+export default gamelogic;
