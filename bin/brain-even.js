cf8e911869563141c4b912b9e090ea188155815c
Alisa Lobanova <17april94@gmail.com>
2022-05-25 23:27:43 +0300

    add brain-calc game - it works!

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,5 +2,4 @@
 
 import even from '../src/even.js';
 
-console.log('Welcome to the Brain Games!');
 even();
---
+++ src/calc.js
@@ -0,0 +1,26 @@
+import gamelogic from './index.js';
+
+const gameCondition = () => {
+  const a = Math.floor(Math.random() * 50 + 1);
+  const b = Math.floor(Math.random() * 50 + 1);
+  let askQuestion = '';
+  let correctAnswer = 0;
+  const randomSign = Math.floor(Math.random() * 3 + 1);
+  if (randomSign === 1) {
+    askQuestion = `${a} + ${b}`;
+    correctAnswer = a + b;
+  }
+  if (randomSign === 2) {
+    askQuestion = `${a} - ${b}`;
+    correctAnswer = a - b;
+  }
+  if (randomSign === 3) {
+    askQuestion = `${a} * ${b}`;
+    correctAnswer = a * b;
+  }
+  return [askQuestion, Number(correctAnswer)];
+};
+
+export default () => {
+  gamelogic('What is the result of the expression?', gameCondition);
+};
